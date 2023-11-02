a56d307d87e5f6be0582e79268599f9cb3fd7797
Yuhao <pyhao@umich.edu>
2023-11-01 21:53:23 -0400

    ADD: DSA-PS2-Q2

---
+++ src/BoxInBox.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class BoxInBox {
+    public BoxInBox() {
+    }
+    private int[] dp;
+    private boolean[] done;
+    public int dpCount(int[] boxes) {
+        int len = boxes.length;
+        dp = new int[len];
+        done = new boolean[len];
+        int max = 0;
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < len; i++) {
+            if (done[i]) {
+                continue;
+            }
+            dp[i] = helper(boxes, i);
+        }
+
+        for (int i = 0; i < len; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    private int helper(int[] boxes, int i) {
+        if (done[i]) {
+            return dp[i];
+        }
+        for (int j = 0; j < boxes.length; j++) {
+            if (j == i) {
+                continue;
+            } else if (boxes[j] < boxes[i]) {
+                dp[i] = Math.max(dp[i], helper(boxes, j) + 1);
+            } else {
+                continue;
+            }
+        }
+        done[i] = true;
+        return dp[i];
+    }
+}
