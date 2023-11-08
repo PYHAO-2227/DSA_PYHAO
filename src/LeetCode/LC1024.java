67f83e77b26920b324194426e39b6dbcb0f72afa
Yuhao <pyhao@umich.edu>
2023-11-08 10:35:27 -0500

    ADD: LC1024

---
+++ src/LeetCode/LC1024.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class LC1024 {
+    public int videoStitching(int[][] clips, int time) {
+        if (time == 0) return 0;
+        Arrays.sort(clips, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+
+        int res = 0;
+        int curEnd = 0, nextEnd = 0;
+        int i = 0, n = clips.length;
+        while (i < n && clips[i][0] <= curEnd) {
+            while (i < n && clips[i][0] <= curEnd) {
+                nextEnd = Math.max(nextEnd, clips[i][1]);
+                i++;
+            }
+
+            res++;
+            curEnd = nextEnd;
+            if (curEnd >= time) {
+                return res;
+            }
+        }
+        return -1;
+    }
+}
