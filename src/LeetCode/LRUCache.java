a4bb8a886b6520de3c46b3105077dd92a470133a
Yuhao <pyhao@umich.edu>
2023-11-09 21:49:04 -0500

    ADD: DIY LRU

---
+++ src/LeetCode/LRUCache.java
@@ -0,0 +1,73 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    private static class Node {
+        int key, value;
+        Node prev, next;
+
+        Node (int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private final int capacity;
+    private final Node dummy = new Node(0, 0);
+    private final Map<Integer, Node> keyToNode = new HashMap<>();
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        dummy.prev = dummy;
+        dummy.next = dummy;
+    }
+
+    public int get(int key) {
+        Node node = getNode(key);
+        return node != null ? node.value : -1;
+    }
+
+    private Node getNode(int key) {
+        if (!keyToNode.containsKey(key)) {
+            return null;
+        }
+
+        Node node = keyToNode.get(key);
+        remove(node);
+        addFront(node);
+        return node;
+    }
+
+    public void put(int key, int value) {
+        Node node = getNode(key);
+        if (node != null) {
+            node.value = value;
+            return;
+        }
+
+        node = new Node(key, value);
+        keyToNode.put(key, node);
+        addFront(node);
+        if (keyToNode.size() > capacity) {
+            Node backNode = dummy.prev;
+            keyToNode.remove(backNode.key);
+            remove(backNode);
+        }
+    }
+
+    private void remove(Node x) {
+        x.prev.next = x.next;
+        x.next.prev = x.prev;
+    }
+
+    private void addFront(Node x) {
+        x.prev = dummy;
+        x.next = dummy.next;
+        x.prev.next = x;
+        x.next.prev = x;
+    }
+}
+
