bab2554a24449039b1692e18067eae942e3eaf6a
Yuhao <pyhao@umich.edu>
2023-11-12 10:18:28 -0500

    ADD: Trie

---
+++ src/LeetCode/Trie.java
@@ -0,0 +1,51 @@
+package LeetCode;
+
+class Trie {
+    private static class TrieNode {
+        boolean val;
+        TrieNode[] children = new TrieNode[26];
+    }
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode curNode = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (curNode.children[i] == null) {
+                curNode.children[i] = new TrieNode();
+                curNode = curNode.children[i];
+            } else {
+                curNode = curNode.children[i];
+            }
+        }
+        curNode.val = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode curNode = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (curNode.children[i] == null) {
+                return false;
+            } else {
+                curNode = curNode.children[i];
+            }
+        }
+        return curNode.val;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode p = root;
+        for (char c : prefix.toCharArray()) {
+            int i = c - 'a';
+            if (p.children[i] == null) return false;
+            p = p.children[i];
+        }
+        return true;
+    }
+}
