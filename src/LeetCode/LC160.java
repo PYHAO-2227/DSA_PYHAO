00254ef775ba75c2cc26b5e2819374c316e5fb8d
Yuhao <pyhao@umich.edu>
2023-11-14 21:28:39 -0500

    ADD: LC160

---
+++ src/LeetCode/LC160.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class LC160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode l = headA, r = headB;
+        while (l != r) {
+            if (l == null) {
+                l = headB;
+            } else {
+                l = l.next;
+            }
+
+            if (r == null) {
+                r = headA;
+            } else {
+                r = r.next;
+            }
+        }
+        return l;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
