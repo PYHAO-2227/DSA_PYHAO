adb9e2c6d28d5d946e67cf7f7fa96e960c34d65d
Yuhao <pyhao@umich.edu>
2023-11-18 09:47:26 -0500

    ADD: lc287-2

---
+++ src/LeetCode/Arr/LC287_2.java
@@ -0,0 +1,21 @@
+package LeetCode.Arr;
+
+public class LC287_2 {
+    // 抽象为环链表
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
