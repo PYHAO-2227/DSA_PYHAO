8f7a9e4ce6bf9fd3265e7bdd2f782e030e4bbef5
Yuhao <pyhao@umich.edu>
2023-11-25 20:25:52 -0500

    ADD: LC136 + 位运算

---
+++ src/LeetCode/BIT/LC136.java
@@ -0,0 +1,11 @@
+package LeetCode.BIT;
+
+public class LC136 {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int n : nums) {
+            res ^= n;
+        }
+        return res;
+    }
+}
