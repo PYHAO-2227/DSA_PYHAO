39612ece6cf2355c168f1e12067d00f26cba2820
Yuhao <pyhao@umich.edu>
2023-11-26 15:04:24 -0500

    ADD: LC1669

---
+++ src/LeetCode/LL/LC1669.java
@@ -0,0 +1,29 @@
+package LeetCode.LL;
+
+import LeetCode.ListNode;
+
+public class LC1669 {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        int gap = b - a + 2;
+        ListNode sentinel = new ListNode(-1, list1);
+
+        ListNode l2end =list2;
+        while (l2end.next != null) {
+            l2end = l2end.next;
+        }
+
+        ListNode slow = list1, fast = slow;
+        for (int i = 0; i < gap; i++) {
+            fast = fast.next;
+        }
+
+        for (int j = 0; j < a - 1; j++) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = list2;
+        l2end.next = fast;
+        return sentinel.next;
+    }
+}
--- src/LeetCode/ListNode.java
+++ src/LeetCode/ListNode.java
@@ -2,7 +2,7 @@ package LeetCode;
 
 public class ListNode {
     int val;
-    ListNode next;
+    public ListNode next;
 
     ListNode(int x) {
         val = x;
