1ecb5aa8849b72ba9afbad097473a0f33b1c6c1a
Yuhao <pyhao@umich.edu>
2023-11-29 21:08:33 -0500

    ADD: TreeNode + 113PathSum

---
+++ src/LeetCode/LC113.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LC113 {
+    List<List<Integer>> res = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        dfs(root, targetSum);
+        return res;
+    }
+
+    private void dfs(TreeNode root, int target) {
+        if (root == null) {
+            return;
+        }
+
+        path.addLast(root.val);
+        target -= root.val;
+        if (root.left == null && root.right == null && target == 0) {
+            res.add(new LinkedList<>(path));
+        }
+        dfs(root.left, target);
+        dfs(root.right, target);
+        path.removeLast();
+    }
+}
