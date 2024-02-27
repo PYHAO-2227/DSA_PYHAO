e0965482df4d35e06addcc7422293e6c9c73a80a
Yuhao <pyhao@umich.edu>
2024-02-27 01:38:22 -0500

    ADD: TIKTOK OA

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
-import AMAZON.DiscountPairs;
+import TIKTOK.findMinProdSum;
 
 // 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
 // 然后按 Enter 键。现在，您可以在代码中看到空格字符。
 public class Main {
     public static void main(String[] args) {
-        // 当文本光标位于高亮显示的文本处时按 Alt+Enter，
-        // 可查看 IntelliJ IDEA 对于如何修正该问题的建议。
-        DiscountPairs testInstance = new DiscountPairs();
-        int[] test1 = new int[]{15,33,15,12,18,15,15};
-        // 按 Shift+F10 或点击装订区域中的绿色箭头按钮以运行脚本。
-
+        int[] testcase = {6, 2, 8, 6, 4};
+//        int[] testcase = {2, 4, 10, 9, 3};
+//        int[] testcase = {1, 5, 9, 4, 10};
         // 按 Shift+F9 开始调试代码。我们已为您设置了一个断点，
         // 但您始终可以通过按 Ctrl+F8 添加更多断点。
-        System.out.println(testInstance.getDiscountPairs(30 ,test1));
-        System.out.println(testInstance.getDiscountPairs1(30 ,test1));
+        findMinProdSum f = new findMinProdSum();
+        System.out.println(f.findRes(testcase));
     }
 }
---
+++ src/TIKTOK/findMinProdSum.java
@@ -0,0 +1,42 @@
+package TIKTOK;
+
+import java.util.Arrays;
+
+public class findMinProdSum {
+    public int findRes(int[] arr) {
+        int len = arr.length;
+        Arrays.sort(arr);
+        int[] newArr = new int[len];
+        int l = 0, r = len - 1;
+        int s = 0, b = len - 1;
+        while (s < b) {
+            newArr[l] = arr[b];
+            l++;
+            b--;
+            newArr[r] = arr[b];
+            r--;
+            b--;
+            if (l >= r) {
+                break;
+            }
+            newArr[l] = arr[s];
+            l++;
+            s++;
+            newArr[r] = arr[s];
+            r--;
+            s++;
+        }
+        if (l == r) {
+            newArr[l] = arr[s];
+        }
+        return findSum(newArr);
+    }
+
+    private int findSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            sum += arr[i] * arr[i + 1];
+        }
+        return sum;
+    }
+}
