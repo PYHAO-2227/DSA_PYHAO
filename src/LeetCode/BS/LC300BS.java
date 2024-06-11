f7d8ce167c0bc92948299dd3ce2354b5ebf41bba
Yuhao <pyhao@umich.edu>
2024-06-11 15:17:36 -0400

    ADD: 300. Longest Increasing Subsequence BINARY SEARCH 边界条件

--- src/LeetCode/BS/LC300BS.java
+++ src/LeetCode/BS/LC300BS.java
@@ -1,4 +1,22 @@
 package LeetCode.BS;
 
 public class LC300BS {
+    public int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int maxL = 0;
+        for (int n : nums) {
+            int left = 0, right = maxL;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (tails[mid] < n) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            tails[left] = n;
+            maxL = Math.max(left + 1, maxL);
+        }
+        return maxL;
+    }
 }
